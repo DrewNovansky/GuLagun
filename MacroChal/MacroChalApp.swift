6aab516f5ae40e40f9860e54ecba997a7b2b0b16
DrewNovansky <novanskyi@gmail.com>
2020-10-01 17:54:37 +0700

    Initial Commit

---
+++ MacroChal/MacroChalApp.swift
@@ -0,0 +1,20 @@
+//
+//  MacroChalApp.swift
+//  MacroChal
+//
+//  Created by Andrew Novansky Ignatius on 01/10/20.
+//
+
+import SwiftUI
+
+@main
+struct MacroChalApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
