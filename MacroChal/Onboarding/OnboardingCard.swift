6aedacc19148a4546a1f1dc3ad54aa8d5257ad37
JeniferYolanda <47961007+JeniferYolanda@users.noreply.github.com>
2020-11-06 14:41:57 +0700

    onboarding fix

--- MacroChal/Onboarding/OnboardingCard.swift
+++ MacroChal/Onboarding/OnboardingCard.swift
@@ -19,4 +19,5 @@ struct OnboardingCard: Identifiable{
 var Data:[OnboardingCard] = [
     OnboardingCard(image: "OnboardingForRest", title: "Welcome to", subtitle: "ForRest!", text: "This is a safe place where you can learn to love yourself more by deepening your connection with yourselves. With ForRest, you are guided to do self-dialogue, to come back to your inner state which is: calm, safe and comfy."),
     OnboardingCard(image: "OnboardingBoo", title: "My name is", subtitle: "BOO", text: "You are not alone in the ForRest, I am here as your personal spirit guide. I will help you to go through this journey to know more about yourself.\nThe fire is my power source.\nSo let's keep the fire going."),
+    OnboardingCard(image: "DevNote", title: "Developers", subtitle: "notes", text: "We care about your privacy and safety.\nWe never collect any text you enter.\nYour text is stored securely in your personal iCloud, not with us."),
 ]
